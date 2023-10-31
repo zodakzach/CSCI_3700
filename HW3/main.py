0ffbd4e0c92f2077a14ffcc0569c9700781cbe2e
Zachary Cervenka <zachary.cervenka@gmail.com>
2023-10-31 17:13:28 -0400

    Fixed unique_fruits function

--- HW3/main.py
+++ HW3/main.py
@@ -26,8 +26,11 @@ def update_basket_a():
         cursor, connection = util.connect_to_db(username, password, host, port, database)
         
         # Insert a new row into basket_a
-        sql_insert = "INSERT INTO basket_a VALUES (5, 'Cherry');"
+        sql_insert = "INSERT INTO basket_a (a, fruit_a) VALUES (5, 'Cherry');"
         success = util.run_and_fetch_sql(cursor, sql_insert)
+
+        #Commits change to database
+        connection.commit()
         
         # Disconnect from the database
         util.disconnect_from_db(connection, cursor)
@@ -39,29 +42,68 @@ def update_basket_a():
         
 @app.route('/api/unique', methods=['GET'])
 def unique_fruits():
-    # Connect to the database
-    cursor, connection = util.connect_to_db(username, password, host, port, database)
-    
-    # Query for unique fruits in basket_a and basket_b
-    sql_query = """
-    SELECT DISTINCT fruit_a FROM basket_a
-    UNION
-    SELECT DISTINCT fruit_b FROM basket_b;
-    """
-    records = util.run_and_fetch_sql(cursor, sql_query)
-    
-    # Disconnect from the database
-    util.disconnect_from_db(connection, cursor)
-    
-    if records == -1:
-        return "Error from PostgreSQL"  # Return error message
-    else:
+    try:
+        # Connect to the database
+        cursor, connection = util.connect_to_db(username, password, host, port, database)
+        
+        # Query for unique fruits in basket_a
+        query_a = "SELECT DISTINCT fruit_a FROM basket_a;"
+        cursor.execute(query_a)
+        unique_fruits_a = [row[0] for row in cursor.fetchall()]
+        
+        # Query for unique fruits in basket_b
+        query_b = "SELECT DISTINCT fruit_b FROM basket_b;"
+        cursor.execute(query_b)
+        unique_fruits_b = [row[0] for row in cursor.fetchall()]
+        
+        # Disconnect from the database
+        util.disconnect_from_db(connection, cursor)
+        
         # Create an HTML table to display unique fruits
-        html_table = "<table><tr><th>Unique Fruits</th></tr>"
-        for record in records:
-            html_table += f"<tr><td>{record[0]}</td></tr>"
+        html_table = """
+          <style>
+                table {
+                    width: 100%;
+                    border-collapse: collapse; /* Collapse the borders */
+                }
+                th, td {
+                    padding: 10px; /* Add padding to cells for spacing */
+                    border-left: 1px solid #000; /* Add a left border to separate columns */
+                }
+                th:first-child,
+                td:first-child {
+                    border-left: none; /* Remove the left border for the first column */
+                }
+            </style>
+
+            <table>
+                <tr>
+                    <th>Unique Fruits in Basket A</th>
+                    <th>Unique Fruits in Basket B</th>
+                </tr>
+            """
+        # Determine the maximum number of rows in both lists
+        max_rows = max(len(unique_fruits_a), len(unique_fruits_b))
+        
+        for i in range(max_rows):
+            html_table += "<tr>"
+            # Display unique fruits in basket_a
+            if i < len(unique_fruits_a):
+                html_table += f"<td>{unique_fruits_a[i]}</td>"
+            else:
+                html_table += "<td></td>"
+            # Display unique fruits in basket_b
+            if i < len(unique_fruits_b):
+                html_table += f"<td>{unique_fruits_b[i]}</td>"
+            else:
+                html_table += "<td></td>"
+            html_table += "</tr>"
+        
         html_table += "</table>"
         return html_table
+    except Exception as e:
+        return f"Error: {e}"
+
 
 if __name__ == '__main__':
 	# set debug mode
